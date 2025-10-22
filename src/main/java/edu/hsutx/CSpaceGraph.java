4fb81fbb015eeda1c06e974c649b727f9b8eafc0
Kailey Pierce <ksp2401@hsutx.edu>
2025-10-22 16:12:47 -0500

    got the code finished, about to start testing

--- src/main/java/edu/hsutx/CSpaceGraph.java
+++ src/main/java/edu/hsutx/CSpaceGraph.java
@@ -2,8 +2,13 @@ package edu.hsutx;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Hashmap;
 
 public class CSpaceGraph extends WeightedDirectedGraph {
+
+    private int[][] cspace;
+    private Map<Point, Integer> pointToVertex = new HashMap<>();
+    private Map<Integer, Point> vertexToPoint = new HashMap<>();
 
     /***
      * Convert a cspace to weighted, directed graph
@@ -17,10 +22,76 @@ public class CSpaceGraph extends WeightedDirectedGraph {
      * @param cspace
      */
     public CSpaceGraph(int[][] cspace) {
-        // TODO - Implement
-        super(0, new ArrayList<Edge>());
+        super(1, new ArrayList<Edge>());
+        this.cspace = cspace;
 
+        int rows = cspace.length;
+        int columns = cspace[0].length;
 
+        ArrayList<Edge> edges = new ArrayList<>();
+
+        int count = 1;
+
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<columns; j++) {
+                if (cspace[i][j] == 0) {
+                    Point p = new Point(i, j);
+                    pointToVertex.put(p, count);
+                    vertexToPoint.put(count, p);
+                    count++;
+                }
+            }
+        }
+
+        int [][] directions = { // N, NE, E, SE, S, SW, W, NW
+            {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}
+        }
+
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<columns; j++) {
+                if (cspace[i][j] == 0) {
+                    Point current = new Point(i, j);
+                    int fromVertex = pointToVertex.get(current);
+
+                    for (int k=0; k<directions.length; k++) {
+                        int x = i + directions[k][0];
+                        int y = i + directions[k][1];
+
+                        if (x >= 0 && x < rows && y >= 0 && y < columns) {
+                            if (cspace[x][y] == 0) {
+                                Point neighbor = new Point(x, y);
+                                int toVertex = pointToVertex.get(neighbor);
+
+                                int dx = directions[k][0];
+                                int dy = directions[k][1];
+                                double weight;
+
+                                if (dx!= 0 && dy != 0) {
+                                    weight = Math.sqrt(2);
+                                }
+                                else {
+                                    weight = 1.0;
+                                }
+
+                                edges.add(new Edge(fromVertex, toVertex, weight));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        super.numVertices = count - 1;
+        super. adjacencyList = new ArrayList<>(count + 1);
+
+        for (int i=0; i<count; i++) {
+            super.adjacencyList.add(new ArrayList<Edge>());
+        }
+
+        for (int i=0 i<edges.size(); i++) {
+            Edge e = edges.get(i);
+            super.adjacencyList.get(e.getStart()).add(e);
+        }
     }
 
     /***
@@ -30,11 +101,20 @@ public class CSpaceGraph extends WeightedDirectedGraph {
      * @return
      */
     public Point[] getDijkstrasPath(Point start, Point end) {
-        // TODO - convert start and end to int vertexes and call the base getDijkstrasPath method
-        // convert the int[] result back to Points, and return the result
-        // The starter code returns a list of points in a straight line from 0,0 to 299,299
-        Point[] pointList = new Point[300];
-        for (int i=0; i<300; i++) pointList[i]=new Point(i,i);
+        Integer start = pointToVertex.get(start);
+        Integer end = pointToVertex.get(end);
+
+        if (start == null || end == null) return new Point[0];
+
+        int[] path = super.getDijkstrasPath(start, end);
+
+        if (path == null) return new Point[0];
+
+        Point[] pointList = new Point[path.length];
+        for (int i=0; i<path.length; i++) {
+            pointList[i] = vertexToPoint.get(path[i]);
+        }
+
         return pointList;
     }
 }
--- src/main/java/edu/hsutx/WeightedDirectedGraph.java
+++ src/main/java/edu/hsutx/WeightedDirectedGraph.java
@@ -1,9 +1,15 @@
 package edu.hsutx;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class WeightedDirectedGraph {
-    // TODO - Implement storage of the graph, including either adjacency list, adjacency matrix, or both.
+    private List<List<Edge>> adjacencyList;
+    private int numVertices;  
 
     /***
      *
@@ -11,8 +17,22 @@ public class WeightedDirectedGraph {
      * @param edgeList: an List of Edges containing start and end vertex # and weight.
      ***/
     public WeightedDirectedGraph(int vertexQuantity, List<Edge> edgeList) {
-        // TODO - Implement
+        this.numVertices = vertexQuantity;
+        adjacencyList = new ArrayList<>(vertexQuantity + 1);
+        int i, j, k; 
+        
+        for (i=0; i<=vertexQuantity; i++) {
+            adjacencyList.add(new ArrayList<Edge>());
+        }
 
+        for (j=0; j<edgeList.size(); j++) {
+            Edge e = edgeList.get(j);
+            int start = e.getStart();
+            int end = e.getEnd();
+            double weight = e.getWeight();
+
+            adjacencyList.get(start).add(e);
+        }
     }
 
     /***
@@ -20,18 +40,40 @@ public class WeightedDirectedGraph {
      * @param start
      * @param end
      */
-    public static boolean isAdjacent(int start, int end) {
-        // TODO - Implement
-        return true;
+    public boolean isAdjacent(int start, int end) {
+        List<Edge> edgesFromStart = adjacencyList.get(start);
+        int i;
+
+        for (i=0; i<edgesFromStart.size(); i++) {
+            Edge e = edgesFromStart.get(i);
+
+            if (e.getEnd() == end) {
+                return true;
+            }
+        } 
+        return false;
     }
 
     /***
      * returns a 2d matrix of adjacency weights, with 0 values for non-adjacent vertices.
      * @return matrix of doubles representing adjacent edge weights
      */
-    public static double[][] adjacencyMatrix() {
-        // TODO - Implement
-        return null;
+    public double[][] adjacencyMatrix() {
+        double [][] weightMatrix = new double[numVertices + 1][numVertices + 1];
+        int i, j;
+        double value = 0;
+        for (i=1; i<adjacencyList.size(); i++) {
+            List<Edge> edgesFromI = adjacencyList.get(i);
+
+            for (j=0; j<edgesFromI.size(); j++) {
+                Edge e = edgesFromI.get(j);
+                value = e.getWeight();
+                int end = e.getEnd();
+
+                weightMatrix[i][end] = value;
+            }
+        }
+        return weightMatrix;
     }
 
     /***
@@ -41,9 +83,55 @@ public class WeightedDirectedGraph {
      * @param end
      * @return an array of integers containing the path of vertices to be traveled, including start and end.
      */
-    public static int[] getBFSPath(int start, int end) {
-        // TODO - Implement
-        return null;
+    public int[] getBFSPath(int start, int end) {
+        boolean [] visited = new boolean[numVertices + 1];
+        int [] parent = new int[numVertices + 1];
+        int i, j, k;
+
+        for (i=0; i<=numVertices; i++) {
+            parent[i] = -1;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            List<Edge> edgesFromCurrent = adjacencyList.get(current);
+
+            for (j=0; j<edgesFromCurrent.size(); j++) {
+                Edge e = edgesFromCurrent.get(j);
+                int neighbor = e.getEnd();
+
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    parent[neighbor] = current;
+                    queue.add(neighbor);
+
+                    if (neighbor == end) {
+                        queue.clear();
+                        break;
+                    }
+                }
+            }
+        }
+        if (!visited[end])  return null;
+
+        List<Integer> pathList = new ArrayList<>();
+        int current = end;
+
+        while (current != -1) {
+            pathList.add(0, current);
+            current = parent[current];
+        }
+
+        int [] path = new int[pathList.size()];
+
+        for (k=0; k<pathList.size();k++) {
+            path[k] = pathList.get(k);
+        }
+        return path;
     }
 
     /***
@@ -53,9 +141,57 @@ public class WeightedDirectedGraph {
      * @param end
      * @return an array of integers containing the path of vertices to be traveled, including start and end.
      */
-    public static int[] getDFSPath(int start, int end) {
-        // TODO - Implement
-        return null;
+    public int[] getDFSPath(int start, int end) {
+      boolean [] visited = new boolean[numVertices + 1];
+        int [] parent = new int[numVertices + 1];
+        int i, j, k;
+
+        for (i=0; i<=numVertices; i++) {
+            parent[i] = -1;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(start);
+
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+
+            if (!visited[current]) {
+                visited[current] = true;
+
+                if (current == end) break;
+                
+                List<Edge> edgesFromCurrent = adjacencyList.get(current);
+                edgesFromCurrent.sort((a, b) -> Integer.compare(a.getEnd(), b.getEnd()));
+
+                for (j=0; j<edgesFromCurrent.size(); j++) {
+                    Edge e = edgesFromCurrent.get(j);
+                    int neighbor = e.getEnd();
+
+                    if (!visited[neighbor]) {
+                        stack.push(neighbor);
+
+                        if (parent[neighbor] == -1) parent[neighbor] = current;
+                    }
+                }
+            }
+        }
+        if (!visited[end])  return null;
+
+        List<Integer> pathList = new ArrayList<>();
+        int current = end;
+
+        while (current != -1) {
+            pathList.add(0, current);
+            current = parent[current];
+        }
+
+        int [] path = new int[pathList.size()];
+
+        for (k=0; k<pathList.size();k++) {
+            path[k] = pathList.get(k);
+        }
+        return path;
     }
 
     /***
@@ -65,12 +201,57 @@ public class WeightedDirectedGraph {
      * @return
      */
     public int[] getDijkstrasPath(int start, int end) {
-        // TODO - Implement Dijkstra's Algorithm
-        // Note
-        return null;
+        double [] distance = new double[numVertices + 1];
+        int [] parent = new int[numVertices + 1];
+        boolean [] visited = new boolean[numVertices + 1];
+        int i, j, k;
+
+        for (i=0; i<=numVertices; i++) {
+            distance[i] = Double.POSITIVE_INFINITY;
+            parent[i] = -1;
+        }
+        distance[start] = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Double.compare(distance[a], distance[b]));
+        pq.add(start);
+
+        while(!pq.isEmpty()) {
+            int current = pq.poll();
+
+            if (visited[current]) continue;
+            visited[current] = true;
+
+            List<Edge> edgesFromCurrent = adjacencyList.get(current);
+            
+            for (j=0; j<edgesFromCurrent.size(); j++) {
+                Edge e = edgesFromCurrent.get(j);
+                int neighbor = e.getEnd();
+                double newDistance = distance[current] + e.getWeight();
+
+                if (newDistance < distance[neighbor]) {
+                    distance[neighbor] = newDistance;
+                    parent[neighbor] = current;
+                    pq.add(neighbor);
+                }
+            }
+        }
+
+        List<Integer> pathList = new ArrayList<>();
+        int current = end;
+
+        while (current != -1) {
+            pathList.add(0, current);
+            current = parent[current];
+        }
+
+        int [] path = new int [pathList.size()];
+        for (k=0; k<pathList.size(); k++) {
+            path[i] = pathList.get(i);
+        }
+
+        if (Double.isInfinite(distance[end])) return null;
+
+        return path;
     }
-
-
-
 }
 
